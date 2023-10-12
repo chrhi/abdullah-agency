0bc15d9fdaf21acf4cef8b2bfd9fb1e70357a676
abdellah <Abdellah.chehri14@gmail.com>
2023-10-12 20:17:08 +0200

    add

---
+++ src/config/site.js
@@ -0,0 +1,37 @@
+export function constructMetadata({
+  title = "Abdullah Agency ",
+  description = "Abdullah Agency is an open-source website.",
+  image = "/agency.PNG",
+  icons = "/favicon.ico",
+  noIndex = false,
+}) {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      creator: "@KING_IN_JUNGLE",
+    },
+    icons,
+    metadataBase: new URL("https://agency-website-abdullah.vercel.app/"),
+    themeColor: "#FFF",
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  };
+}
